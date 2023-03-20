5ed32368627e91eeccb6d3527aa70bebe09d3e17
Alex <alexallafidev.@gmail.com>
2023-03-20 19:01:11 +0200

    Change `SetsOperationTests` structure for correct gradle testing output

--- src/test/kotlin/model/SetsOperationsTests.kt
+++ src/test/kotlin/model/SetsOperationsTests.kt
@@ -32,26 +32,25 @@ class SetsOperationsTests {
         assertEquals(oddsUpToOneMillion(), testResult)
     }
 
-}
+    fun oneMillion(): Set<Int> {
+        return (0..1_000_000)
+            .fold(mutableSetOf()) { acc, i -> acc.add(i); acc }
+    }
 
-fun oneMillion(): Set<Int> {
-    return (0..1_000_000)
-        .fold(mutableSetOf()) { acc, i -> acc.add(i); acc }
-}
+    fun evensUpToOneMillion(): Set<Int> {
+        return (0..1_000_000)
+            .filter { it.isEven }
+            .fold(mutableSetOf()) { acc, i -> acc.addAndReturnSelf(i) }
+    }
 
-fun evensUpToOneMillion(): Set<Int> {
-    return (0..1_000_000)
-        .filter { it.isEven }
-        .fold(mutableSetOf()) { acc, i -> acc.addAndReturnSelf(i) }
-}
+    fun oddsUpToOneMillion(): Set<Int> {
+        return (0..1_000_000)
+            .filter { it.isOdd }
+            .fold(mutableSetOf()) { acc, i -> acc.addAndReturnSelf(i) }
+    }
 
-fun oddsUpToOneMillion(): Set<Int> {
-    return (0..1_000_000)
-        .filter { it.isOdd }
-        .fold(mutableSetOf()) { acc, i -> acc.addAndReturnSelf(i) }
-}
-
-fun <T : Any> MutableSet<T>.addAndReturnSelf(t: T): MutableSet<T> {
-    this += t
-    return this
+    fun <T : Any> MutableSet<T>.addAndReturnSelf(t: T): MutableSet<T> {
+        this += t
+        return this
+    }
 }
