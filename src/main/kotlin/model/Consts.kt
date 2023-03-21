e77d69552d9aa6b564005e25fd6bc2f1ac574d56
Alex <alexallafi.dev@gmail.com>
2023-03-21 22:36:37 +0200

    Cleanup deprecated implementation

--- src/main/kotlin/model/Consts.kt
+++
@@ -1,4 +0,0 @@
-const val LOG_CONSTRUCT_DIRECTORY_TREE = "Constructing directory tree for: "
-const val LOG_CONSTRUCTION_COMPLETED = "Directory tree construction completed. Files found: "
-const val LOG_COMPUTING_DIFFERENCES = "Computing differences:"
-const val LOG_CONSTRUCTION_DIRECTORY_TREES = "Constructing directory trees"
--- src/test/kotlin/testUtils.kt
+++
@@ -1,3 +0,0 @@
-import domain.UserInput
-
-val fakeUserInput = UserInput("src/test/resources/search_for_files_from", "src/test/resources/search_for_files_in")
